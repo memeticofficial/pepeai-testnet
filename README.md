b31721fe9d7948138d876e5374ce84c686d674c7
cghost <cghost>
2023-05-07 17:20:09 -0400

    PepeAI chatbot poc
    - readme.md update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # pepeai-testnet
+- experimental testnet for decentralized p2p AI processing
+
+
+
+Setting up the chatbot environment:
+
+```
+conda install pytorch pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install -r requirements.txt
+python3 ./pepeaichatbot.py
+```
+
+Running a Node:
+
+PepeAI net seperates processing by PepeCoin Nodes.  Each node requires at least 1 GPU w/ 8GB+ or more of memory.
+
+- bloom model layers distrubuted across fleet of pepecoin nodes
+- will support various models as they are updated for pepecoin ai net
+
---
+++ pepeaichatbot.py
@@ -0,0 +1,31 @@
+from petals import DistributedBloomForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+initial_peers = ['/ip4/pepeaitestnet.pepeco.in/tcp/8989/p2p/Qme6JYqrWzV7NF1Et8er4mseXUomzbLtedbNqHdw4oie7G']
+
+model = DistributedBloomForCausalLM.from_pretrained("bigscience/bloomz-petals", tuning_mode="ptune", pre_seq_len=16, initial_peers=initial_peers)
+tokenizer = AutoTokenizer.from_pretrained(testmodel)
+
+# streaming chat bot
+with model.inference_session(max_length=512) as sess:
+    while True:
+        prompt = input('Human: ')
+        if prompt == "":
+            break
+        prefix = f"Human: {prompt}\nPepeAI:"
+        prefix = tokenizer(prefix, return_tensors="pt")["input_ids"]
+        print("PepeAI:", end="", flush=True)
+        
+        while True:
+            outputs = model.generate(
+                prefix, max_new_tokens=1, do_sample=True, top_p=0.9, temperature=0.75, session=sess
+            )
+            outputs = tokenizer.decode(outputs[0, -1:])
+            print(outputs, end="", flush=True)
+            if "\n" in outputs:
+                break
+            prefix = None  # Prefix is passed only for the 1st token of the bot's response
+
+
+
